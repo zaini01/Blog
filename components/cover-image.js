ddb65b2ac776d7e6b9d60001ee3f8ab62edbb53a
zaini01 <o.z41n1@gmail.com>
2021-11-24 10:28:44 +0700

    fix image

--- components/cover-image.js
+++ components/cover-image.js
@@ -4,16 +4,17 @@ import Image from 'next/image'
 
 export default function CoverImage({ title, src, slug, height, width }) {
   const image = (
-    <Image
-      src={src}
-      alt={`Cover Image for ${title}`}
-      className={cn('shadow-sm', {
-        'hover:shadow-md transition-shadow duration-200': slug,
-      })}
-      layout="responsive"
-      width={width}
-      height={height}
-    />
+    <img src={src} alt="Italian Trulli"></img>
+    // <Image
+    //   src={src}
+    //   alt={`Cover Image for ${title}`}
+    //   className={cn('shadow-sm', {
+    //     'hover:shadow-md transition-shadow duration-200': slug,
+    //   })}
+    //   layout="responsive"
+    //   width={width}
+    //   height={height}
+    // />
   )
   return (
     <div className="sm:mx-0">
